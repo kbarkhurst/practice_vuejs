29379c5bf9df48bac8d39fb99200c1d2382eb749
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-23 09:51:15 -0400

    displaying data using axios and vue

--- testbtn.js
+++ testbtn.js
@@ -9,17 +9,26 @@ var app = new Vue({
   data: function() {
     return {
       message: "Hello from JavaScript!",
-      todos: []
+      todos: [],
+      title: ""
     };
   },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then(response => {
+        console.log(response.data);
+        this.todos = response.data;
+      });
+  },
   methods: {
-    loadTodos: function() {
-      axios
-        .get("https://jsonplaceholder.typicode.com/todos")
-        .then(response => {
-          console.log(response.data);
-          this.todos = response.data;
-        });
+    createTodo: function() {
+      var params = { userId: 1, title: this.title, completed: false};
+      axios.post("https://jsonplaceholder.typicode.com/todos", params).then(response => { 
+        console.log(response.data);
+        this.todos.push(response.data);
+        this.title = "";
+      });
     }
   }
 });
